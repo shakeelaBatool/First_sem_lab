c3c8f55e88498b9e20cb6c14484c390b9eb7e4c5
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-14 10:48:13 +0500

    Update compares_two_string.py

--- compares_two_string.py
+++ compares_two_string.py
@@ -1,10 +1,11 @@
 # Write a program that compares two strings and prints whether they are equal or not.
-name1=input("Write here anything: ")
-name2=input("Write here anyother string: ")
+name1 = input("Write here anything: ")
+name2 = input("Write here anyother string: ")
 j=name2
 Length1=len(name1)
 Length2=len(name2)
 if Length1==Length2:
     print(name1," and ",name2,"  are equal")
 else:
+
     print(name1," and ",name2,"  not are equal") 
